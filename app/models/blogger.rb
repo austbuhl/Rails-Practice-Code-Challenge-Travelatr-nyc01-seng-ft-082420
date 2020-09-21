5f20b4fe88ce53618a31e9b5317d330a876b3a68
github username <github email address>
2020-09-21 15:14:36 -0400

    second take

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,4 +1,10 @@
 class Blogger < ApplicationRecord
     has_many :posts
     has_many :destinations, through: :posts
+
+    def number_of_likes
+        self.posts.sum do |p|
+            p.likes 
+        end
+    end
 end
